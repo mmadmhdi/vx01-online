031c930ef133319c10d70ba483e1df1ae72cf767
mmadmhdi <mmd85mmd@gmail.com>
2026-05-28 22:25:12 +0330

    Delete client directory

--- client/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import '@livekit/components-styles'
-import './style.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<App />)
